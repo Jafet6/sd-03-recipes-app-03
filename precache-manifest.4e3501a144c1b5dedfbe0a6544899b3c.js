a6cdab499f89199fa1eeb3613e8f609c168b7758
Jafet Henrique <jafetguerra6@gmail.com>
2020-07-09 12:17:34 -0300

    Updates

---
+++ precache-manifest.4e3501a144c1b5dedfbe0a6544899b3c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93278954d8d5acdcc445a7f040bea093",
+    "url": "/sd-03-recipes-app-03/index.html"
+  },
+  {
+    "revision": "5873fe631d439de17ad0",
+    "url": "/sd-03-recipes-app-03/static/css/main.a1954c0e.chunk.css"
+  },
+  {
+    "revision": "510037aa8aa49768fc2e",
+    "url": "/sd-03-recipes-app-03/static/js/2.43848cab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sd-03-recipes-app-03/static/js/2.43848cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5873fe631d439de17ad0",
+    "url": "/sd-03-recipes-app-03/static/js/main.55df94e9.chunk.js"
+  },
+  {
+    "revision": "74e09fdf68f4f7bd7c66",
+    "url": "/sd-03-recipes-app-03/static/js/runtime-main.01b01e31.js"
+  },
+  {
+    "revision": "3c35e11f11ac4527aa692314ee71094f",
+    "url": "/sd-03-recipes-app-03/static/media/beber.3c35e11f.svg"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/sd-03-recipes-app-03/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "b9ef5071bb605cc7440d599ed599f969",
+    "url": "/sd-03-recipes-app-03/static/media/chef.b9ef5071.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/sd-03-recipes-app-03/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/sd-03-recipes-app-03/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "8bab732c217955f87f5a9ae437ce9469",
+    "url": "/sd-03-recipes-app-03/static/media/garfo.8bab732c.svg"
+  },
+  {
+    "revision": "8582886ca612029ccd7bf3cf071c9559",
+    "url": "/sd-03-recipes-app-03/static/media/icones.8582886c.png"
+  },
+  {
+    "revision": "bd9121aaac3294795f1cc5daf0aa5b0f",
+    "url": "/sd-03-recipes-app-03/static/media/loginbackground.bd9121aa.gif"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/sd-03-recipes-app-03/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "b9ef5071bb605cc7440d599ed599f969",
+    "url": "/sd-03-recipes-app-03/static/media/profileIcon.b9ef5071.svg"
+  },
+  {
+    "revision": "a852f82b4de845ab6d4963f16ac0a443",
+    "url": "/sd-03-recipes-app-03/static/media/searchIcon.a852f82b.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/sd-03-recipes-app-03/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/sd-03-recipes-app-03/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/precache-manifest.5466bb0709c5ec8c737daf76b648f4d8.js"
+  "/sd-03-recipes-app-03/precache-manifest.4e3501a144c1b5dedfbe0a6544899b3c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sd-03-recipes-app-03/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
